131f4271dc084c9a3efd0cf6be2ecdf3072cd0ee
James Newman <jamesmnewman1@gmail.com>
2014-03-17 20:56:41 -0700

    seeing if this works

--- FieldButton/ViewController.m
+++ FieldButton/ViewController.m
@@ -9,6 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *thePlace;
+@property (weak, nonatomic) IBOutlet UITextField *theVerb;
+@property (weak, nonatomic) IBOutlet UITextField *theNumber;
+@property (weak, nonatomic) IBOutlet UITextView *theTemplate;
+@property (weak, nonatomic) IBOutlet UITextView *theStory;
+- (IBAction)createStory:(id)sender;
+
+- (IBAction)hideKeyboard:(id)sender;
 
 @end
 
@@ -26,4 +34,18 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)createStory:(id)sender {
+    self.theStory.text = [self.theTemplate.text stringByReplacingOccurrencesOfString:@"<place>" withString: self.thePlace.text];
+    self.theStory.text = [self.theStory.text stringByReplacingOccurrencesOfString:@"<verb>" withString:self.theVerb.text];
+    self.theStory.text = [self.theStory.text stringByReplacingOccurrencesOfString:@"<number>" withString:self.theNumber.text];
+}
+
+
+- (IBAction)hideKeyboard:(id)sender {
+    //place, verb, number, template
+    [self.thePlace resignFirstResponder];
+    [self.theVerb resignFirstResponder];
+    [self.theNumber resignFirstResponder];
+    [self.theTemplate resignFirstResponder];
+}
 @end
